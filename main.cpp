6abe6b45a357bbe887c185e09e8032b77d97c319
Michael Mitchell <michaelmatthewmitchell@live.com>
2015-04-12 00:13:46 -0700

    Trying to fix race condition in PushBack/PopFront

--- main.cpp
+++ main.cpp
@@ -7,35 +7,41 @@
 
 #include "atomic_queue.hpp"
 
+#define SIZE 1000000u
 
 int main(){
 
-	std::vector<std::thread> threads;
+	for (auto tests = 0u; tests < 100u; ++tests){
+		std::vector<std::thread> threads;
 
-	auto aq = MMM::AtomicQueue<unsigned>();
+		auto aq = MMM::AtomicQueue<unsigned>();
 
-	auto count = 5u;
+		std::atomic_uint32_t counter{ 0u };
+		auto count = 0u;
 
-	auto func = 
-		[&]()
-	{
-		for (auto i = 0u; i < 10000; ++i){
-			aq.PushBack(count);
+		auto func =
+			[&]()
+		{
+			for (auto i = 0u; i < SIZE; ++i){
+				count = counter.fetch_add(1u);
+				aq.PushBack(count);
+			}
+		};
+
+		for (auto i = 0u; i < 8u; ++i){
+			threads.push_back(std::thread{ func });
 		}
-	};
 
-	for (auto i = 0u; i < 8u; ++i){
-		threads.push_back(std::thread{ func });
+		for (auto i = 0u; i < 8u; ++i){
+			threads[i].join();
+		}
+
+		for (auto i = 0u; i < 50; ++i){
+			//printf("%u : %u\n", i, aq.PopFront());
+		}
+		
+		printf("test # %u\n", tests);
 	}
-
-	for (auto i = 0u; i < 8u; ++i){
-		threads[i].join();
-	}
-
-	for (auto i = 0u; i < 20; ++i){
-		printf("%u : %u\n", i, aq.PopFront());
-	}
-
 
 	std::cin.ignore(1000, '\n');
 	return 0;
